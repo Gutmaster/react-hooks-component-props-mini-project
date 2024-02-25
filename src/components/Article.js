e4b08b926e18898abc4b9bb22772274c7805aa54
+ <+>
2024-02-25 05:42:13 -0800

    completer lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-function Article({title, date = "January 1, 1970", preview}){
+function Article({title, date = "January 1, 1970", preview, minutes}){
+    let emojis = ''
+    if(minutes < 30){
+        for(let i = 0; i < Math.ceil(minutes/5); i++){
+            emojis += '☕️'
+        }
+    }else{
+        for(let i = 0; i < Math.ceil(minutes/10); i++){
+            emojis += '🍱'
+        }
+    }
     return(
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
+            <p>{emojis}</p>
             <p>{preview}</p>
         </article>
     )
